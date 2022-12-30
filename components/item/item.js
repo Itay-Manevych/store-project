7424b1c53ff4e2e63916fe0102b2f0cc7fdf8983
ItayFrisk <itay.manevych@gmail.com>
2022-12-30 20:32:04 +0200

    (patch) added visibillity

--- components/item/item.js
+++ components/item/item.js
@@ -1,7 +1,11 @@
+import { itemsJson } from "/components/item/itemjson.js";
+
 export default function Item()
 {
+    const obj = JSON.parse(itemsJson);
     const item = document.createElement("div");
     item.setAttribute("id","item");
+
     // Create new link Element
     const link = document.createElement('link');
 
@@ -14,6 +18,32 @@ export default function Item()
 
     // Append link element to HTML head
     item.appendChild(link);
+
+    const CreateNode = (elementType, value, extraText)=>{
+        const node = document.createElement(`${elementType}`);
+        node.setAttribute("id",`${elementType}`);
+        if(elementType === "img")
+        {
+            node.src = value;
+        }
+        
+        else
+        {
+            node.innerHTML = extraText + value;
+        }
+            
+        return node;
+    }
+
+   
+    item.appendChild(CreateNode("img",obj.items[0].img));
+    item.appendChild(CreateNode("p",obj.items[0].title, "Title: "));
+    item.appendChild(CreateNode("p",obj.items[0].description, "Description: "));
+    item.appendChild(CreateNode("p",obj.items[0].category, "Category: "));
+    item.appendChild(CreateNode("p",obj.items[0].price, "Price: "));
+    // img.setAttribute("id", "image")
+    // img.src = obj.items[0].img;
+    // item.appendChild(img);
     return item;
 }
 
---
+++ components/item/itemjson.js
@@ -0,0 +1,4 @@
+export const itemsJson = '{ "items" : [' + 
+'{ "img":"components/item/itemImages/Smiley.png",  "id":0, "title":"2", "description":"3", "category": "4", "price":0 },' +
+'{ "img":"components/item/itemImages/Smiley.png",  "id":0, "title":"2", "description":"3", "category": "4", "price":0 },' +
+'{ "img":"components/item/itemImages/Smiley.png",  "id":0, "title":"2", "description":"3", "category": "4", "price":0 } ]}';
